ea9267917ea026a75d74c1d4ed6783534dd30569
Arihant Jain <arihantpro4321@gmail.com>
2026-04-08 16:15:46 +0530

    Refactor salary calculation variables to uppercase

--- gross salary.cpp
+++ gross salary.cpp
@@ -3,27 +3,27 @@
 int main(){
 
     float salary;
-    float hra_percent, da_percent, ta_percent;
-    float hra, da, ta, gross;
+    float HRA,DA,TA;
+   
 
     printf("Enter Salary: ");
     scanf("%f", &salary);
 
   
     printf("Enter HRA percentage: ");
-    scanf("%f", &hra_percent);
-    hra = (hra_percent / 100) * salary;
+    scanf("%f", &HRA);
+    hra = (HRA / 100) * salary;
     printf("HRA = %f\n", hra);
 
     
     printf("Enter DA percentage: ");
-    scanf("%f", &da_percent);
-    da = (da_percent / 100) * salary;
+    scanf("%f", &DA);
+    da = (DA / 100) * salary;
     printf("DA = %f\n", da);
 
     printf("Enter TA percentage: ");
-    scanf("%f", &ta_percent);
-    ta = (ta_percent / 100) * salary;
+    scanf("%f", &TA);
+    ta = (TA / 100) * salary;
     printf("TA = %f\n", ta);
 
     
