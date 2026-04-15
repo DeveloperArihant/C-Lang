85cacac0faaf37b88e4782a01abe5799f70483de
Arihant Jain <arihantpro4321@gmail.com>
2026-04-15 15:57:26 +0530

    Add files via upload

---
+++ looop square.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ main(){
+	
+	int a,b;
+	printf("Enter the number ");
+	scanf("%d %d", &a, &b);
+	
+	while(a<=b)
+	{
+	    printf("%d - %d\n",a ,a*a);
+		a++	;
+		
+		
+	}
+	
+}
